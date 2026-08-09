b7f4750c2219d4e1ac54ce8498e662d727331927
BlendarOrana <blendorana1@hotmail.com>
2026-08-09 12:50:01 +0200

    asdasdsa

--- frontend/src/components/admin/IphoneMockup.jsx
+++ frontend/src/components/admin/IphoneMockup.jsx
@@ -4,7 +4,7 @@ export default function IphoneMockup({ children }) {
   return (
     <div className="relative w-[320px] h-[660px] bg-black rounded-[50px] p-[10px] flex-shrink-0 shadow-2xl">
       {/* Ekrani i brendshem */}
-      <div className="relative w-full h-full bg-[#fcee0d] rounded-[38px] overflow-hidden">
+      <div className="relative w-full h-full bg-white rounded-[38px] overflow-hidden">
         
         {/* Imazhi i sfondit (Erresuar pak per te nxjerre ne pah tekstin e bardhe) */}
         <div className="absolute inset-0 bg-[url('/hunters.webp')] bg-contain bg-center bg-no-repeat z-0"></div>
